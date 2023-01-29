e18e9b38efb25ac524d5aacde56bb7595b8e77b8
Heil Park <heilpark@me.com>
2023-01-29 18:16:32 +0000

    layout and saving working

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,49 +5,70 @@ var projectDisplayEl = $('.container');
 var currDate = moment().format("dddd, MMMM Do");
 timeDisplayEl.text(currDate);
   
-// for (let i = 0; i < 9; i++) {
-//     var thisHr = i + 9;
-//     var sss = $("<tr>").addClass("row time-block");
+for (let i = 0; i < 9; i++) {
+    // sets the hour for this 
+    var thisHr = i + 9;
 
-//     var desc = $("<textarea>").addClass("col-lg-10");
+    // adds table row
+    var sss = $("<tr>").addClass("row time-block");
 
-//     if (moment().format("H") > thisHr) {
-//         sss.addClass("past");
-//     } else if (moment().format("H") <  thisHr) {
-//         sss.addClass("future");
-//     } else {
-//         sss.addClass("present");
-//     }
+    // gets current hour and compares against this hour in the loop
+    if (moment().format("H") > thisHr) {
+        sss.addClass("past");
+    } else if (moment().format("H") <  thisHr) {
+        sss.addClass("future");
+    } else {
+        sss.addClass("present");
+    }
+    sss.attr("hr", thisHr);
 
-//     // if in localstorage then show
-//     if (localStorage.getItem("hour-" + thisHr) ) {
-//         desc.text(localStorage.getItem("hour-" + thisHr));
-//     } else {
-//         desc.text("");
-//     }
+    // adds text area for this timeblock
+    var desc = $("<textarea>").addClass("col-lg-10");
 
-//     var hr = $('<td>').addClass('col-lg-1 hour').text(thisHr);
-//     hr.attr("hr", thisHr);
+    // if in localstorage then show
+    if (localStorage.getItem("hour-" + thisHr) ) {
+        desc.text(localStorage.getItem("hour-" + thisHr));
+    } else {
+        desc.text("");
+    }
 
-//     var saveProjectBtn = $('<i>').addClass('fa-solid fa-floppy-disk ');
-//     var btn  = $('<button>').addClass("block");
-//     btn.attr("type","button");
-//     var saveProject  = $('<td>').addClass('saveBtn col-lg-1');  
+    // adds cell for the hour
+    var hr = $('<td>').addClass('col-lg-1 hour');
 
-//     btn.append(saveProjectBtn);
-//     saveProject.append(btn);
-//     sss.append(hr, desc, saveProject);
-//     projectDisplayEl.append(sss);
-// }
+    // adds correct AM OR PM
+    if (thisHr < 12) {
+        hr.text(thisHr + "AM");
+    } else if (thisHr > 12) {
+        hr.text((thisHr - 12) + "PM");
+    } else {
+        hr.text(thisHr + "PM");
+    }
+     
+    // adds cell for save button
+    var saveProjectBtn = $('<i>').addClass('fa-solid fa-floppy-disk ');
+    var btn  = $('<button>').addClass("block ");
+    btn.attr("type","button");
+    var saveProject  = $('<td>').addClass('col-lg-1 saveBtn '); 
+
+    // appends button to saveBtn cell
+    btn.append(saveProjectBtn);
+    saveProject.append(btn);
+
+    // appends to table row and then display
+    sss.append(hr, desc, saveProject);
+    projectDisplayEl.append(sss);
+}
  
-// function handleSave(event) {
-//     var targ = event.target;
+function handleSave(event) {
+    // gets target of click and the parent timeblock
+    var targ = $(event.target);
+    var outerTR = targ.closest('tr');
  
-//     var hour = targ.attr("hr");
-//     var content = targ.val().trim();
-//     localStorage.setItem("hour-" + hour, content);
-  
- 
-// }
+    // takes the hour and text from the triggered timeblock
+    var hour = outerTR.attr("hr");
+    var content = outerTR.children('textarea').val().trim();
 
-// $("button").on('click', handleSave);
+    localStorage.setItem("hour-" + hour, content);
+}
+
+$("button").on('click', handleSave);
